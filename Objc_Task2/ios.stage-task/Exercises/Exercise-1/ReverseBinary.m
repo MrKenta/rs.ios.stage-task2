105de6aa8ae2ebb543afc3f23fef8715a8f07778
Raman Krutsiou <ramankrutsiou@MacBook-Pro-Raman.local>
2021-05-16 20:21:24 +0300

    Add first task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,29 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+   
+    if (n == 0) {
+        return n ;
+    }
+    
+    int input = n;
+
+    UInt8 result = 0;
+    
+    NSMutableArray *arrayNumber = [NSMutableArray new];
+    
+    while (input != 0) {
+        
+        int remaning  = input % 2;
+            [arrayNumber addObject:[NSNumber numberWithInt:remaning]];
+        int value = input / 2;
+        input = value;
+    }
+    int myltiplyer = 128;
+        for (int i = 0; i <= arrayNumber.count -1 ; i++) {
+            int number = [[arrayNumber objectAtIndex:i]intValue] * myltiplyer;
+            result += number;
+            myltiplyer /= 2;
+        }
+    return  result;
 }
